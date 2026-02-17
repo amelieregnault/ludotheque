b7966de07e2b88f64635d47f70e9f475d76450ff
amelieregnault <amelie.regnault@laposte.net>
2026-02-17 09:27:21 +0100

    Category - Fixtures - Modif

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -16,24 +16,28 @@ class CategoryFixtures extends Fixture
         $category->setDescription("Un jeu de société est une activité ludique qui réunit 
             plusieurs participants autour d’un objectif commun, souvent pour s’amuser, 
             se divertir ou apprendre.");
+        $this->addReference("cat1", $category);
         $manager->persist($category);
         
         // Catégorie 2
         $category = new Category();
         $category->setName("Jeux éducatifs");
         $category->setDescription("Un jeu éducatif est une activité ludique conçue pour permettre aux participants d’acquérir de nouvelles connaissances ou de développer des compétences spécifiques");
+        $this->addReference("cat2", $category);
         $manager->persist($category);
         
         // Catégorie 3
         $category = new Category();
         $category->setName("Jeux vidéos");
         $category->setDescription("Un jeu vidéo est un jeu électronique doté d'une interface utilisateur permettant une interaction humaine ludique en générant un retour visuel sur un dispositif vidéo.");
+        $this->addReference("cat3", $category);
         $manager->persist($category);
         
         // Categorie 4
         $category = new Category();
         $category->setName("Jeux de rôle");
         $category->setDescription("Le jeu de rôle est une activité où les participants incarnent un personnage et interagissent dans un univers réel ou fictif, influençant l’histoire par leurs actions et décisions.");
+        $this->addReference("cat4", $category);
         $manager->persist($category);
 
         $manager->flush();
